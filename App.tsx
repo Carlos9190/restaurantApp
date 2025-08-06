59aa22615f3af6535258d6a91082094837e51336
Carlos Eduardo Ibarra Manzano <carlosibarra999@outlook.com>
2025-08-06 02:06:44 -0500

    feat: add react paper

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import 'react-native-gesture-handler';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+// State from context
+import FirebaseState from './src/context/firebase/FirebaseState';
+import OrderState from './src/context/orders/OrdersState';
 
 import NewOrder from './src/views/NewOrder';
 import Menu from './src/views/Menu';
@@ -10,71 +11,85 @@ import DishForm from './src/views/DishForm';
 import OrderResume from './src/views/OrderResume';
 import OrderProgress from './src/views/OrderProgress';
 
-// State from context
-import FirebaseState from './src/context/firebase/FirebaseState';
-import OrderState from './src/context/orders/OrdersState';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
-const Stack = createStackNavigator();
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { RootStackParamList } from './src/types';
+const Stack = createStackNavigator<RootStackParamList>();
+
+// Theme
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#FFDA00',
+  },
+};
 
 export default function App() {
   return (
     <FirebaseState>
       <OrderState>
-        <NavigationContainer>
-          <Stack.Navigator
-            screenOptions={{
-              headerStyle: {
-                backgroundColor: '#FFDA00',
-              },
-              headerTitleStyle: {
-                fontWeight: 'bold',
-              },
-            }}
-          >
-            <Stack.Screen
-              name="NewOrder"
-              component={NewOrder}
-              options={{
-                title: 'New order',
+        <PaperProvider>
+          <NavigationContainer>
+            <Stack.Navigator
+              screenOptions={{
+                headerStyle: {
+                  backgroundColor: theme.colors.primary,
+                },
+                headerTitleStyle: {
+                  fontWeight: 'bold',
+                },
+                headerTitleAlign: 'center',
+                headerTintColor: '#000',
               }}
-            />
-            <Stack.Screen
-              name="Menu"
-              component={Menu}
-              options={{
-                title: 'Our menu',
-              }}
-            />
-            <Stack.Screen
-              name="DishDetails"
-              component={DishDetails}
-              options={{
-                title: 'Dish details',
-              }}
-            />
-            <Stack.Screen
-              name="DishForm"
-              component={DishForm}
-              options={{
-                title: 'Order dish',
-              }}
-            />
-            <Stack.Screen
-              name="OrderResume"
-              component={OrderResume}
-              options={{
-                title: 'Order resume',
-              }}
-            />
-            <Stack.Screen
-              name="OrderProgress"
-              component={OrderProgress}
-              options={{
-                title: 'Order progress',
-              }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
+            >
+              <Stack.Screen
+                name="NewOrder"
+                component={NewOrder}
+                options={{
+                  title: 'New order',
+                }}
+              />
+              <Stack.Screen
+                name="Menu"
+                component={Menu}
+                options={{
+                  title: 'Our menu',
+                }}
+              />
+              <Stack.Screen
+                name="DishDetails"
+                component={DishDetails}
+                options={{
+                  title: 'Dish details',
+                }}
+              />
+              <Stack.Screen
+                name="DishForm"
+                component={DishForm}
+                options={{
+                  title: 'Order dish',
+                }}
+              />
+              <Stack.Screen
+                name="OrderResume"
+                component={OrderResume}
+                options={{
+                  title: 'Order resume',
+                }}
+              />
+              <Stack.Screen
+                name="OrderProgress"
+                component={OrderProgress}
+                options={{
+                  title: 'Order progress',
+                }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </PaperProvider>
       </OrderState>
     </FirebaseState>
   );
--- src/views/NewOrder.tsx
+++ src/views/NewOrder.tsx
@@ -1,5 +1,32 @@
-import { Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { globalStyles } from '../styles';
+import { RootStackParamList } from '../types';
+
+type NavigationProp = StackNavigationProp<RootStackParamList, 'NewOrder'>;
 
 export default function NewOrder() {
-  return <Text>NewOrder</Text>;
+  const navigation = useNavigation<NavigationProp>();
+
+  return (
+    <View style={globalStyles.container}>
+      <View style={[globalStyles.content, styles.content]}>
+        <Button
+          style={globalStyles.btn}
+          onPress={() => navigation.navigate('Menu')}
+        >
+          <Text style={globalStyles.btnText}>Create new order</Text>
+        </Button>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  content: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+});

84e103aa67e55f8eebbf0899e64729a5c7b3910b
Carlos Eduardo Ibarra Manzano <carlosibarra999@outlook.com>
2025-08-06 18:17:48 -0500

    feat: creating dish form content and adding react native vector icons

--- src/views/DishForm.tsx
+++ src/views/DishForm.tsx
@@ -1,5 +1,67 @@
-import { Text } from 'react-native';
+import { useContext } from 'react';
+import { Image, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import {
+  Button,
+  Card,
+  IconButton,
+  Surface,
+  Text,
+  TextInput,
+} from 'react-native-paper';
+import OrderContext from '../context/orders/ordersContext';
+import { imageMap } from '../../assets';
+import { globalStyles } from '../styles';
+import { RootStackParamList } from '../types';
+
+type NavigationProp = StackNavigationProp<RootStackParamList, 'DishForm'>;
 
 export default function DishForm() {
-  return <Text>DishForm</Text>;
+  return (
+    <View style={globalStyles.container}>
+      <View style={globalStyles.content}>
+        <View style={{ padding: 16 }}>
+          <Text style={globalStyles.title}>Quantity</Text>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={{ flex: 1 }}>
+              <Button
+                style={{
+                  height: 80,
+                  justifyContent: 'center',
+                  backgroundColor: '#000',
+                }}
+              >
+                <IconButton icon="minus" size={40} iconColor="#FFF" />
+              </Button>
+            </View>
+            <View style={{ flex: 1 }}>
+              <TextInput
+                style={{
+                  textAlign: 'center',
+                  fontSize: 20,
+                  backgroundColor: 'transparent',
+                  padding: 12,
+                }}
+                underlineColor="transparent"
+                activeUnderlineColor="transparent"
+                value="1"
+              />
+            </View>
+            <View style={{ flex: 1 }}>
+              <Button
+                style={{
+                  height: 80,
+                  justifyContent: 'center',
+                  backgroundColor: '#000',
+                }}
+              >
+                <IconButton icon="plus" size={40} iconColor="#FFF" />
+              </Button>
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 }
